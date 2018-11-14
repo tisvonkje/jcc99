efe56e91d9175d9114274db6d2e24eeed492e436
Franc Grootjen <franc@me.com>
2018-11-15 00:39:58 +0100

    first attempt for entry function

--- jcc99/entry.c
+++ jcc99/entry.c
@@ -11,94 +11,70 @@
  */
 extern void *heapptr;
 /*
- * Convert C-string to String
+ * Convert a C-string to a character array
  */
-void *toString(char *cstring)
-{
-	/*
-	 * Allocate storage for String
-	 */
-	void *result=heapptr;
-	heapptr+=WORDSIZE;
-	/*
-	 * Allocate storage for the char array
-	 */
-	void *array=heapptr;
-	heapptr+=WORDSIZE+strlen(cstring)*2;
-	/*
-	 * Keep heap on WORD boundary
-	 */
-	heapptr=(heapptr+WORDSIZE-1)&WORDMASK;
-	/*
-	 * Store array in String
-	 */
-	*((void **)result)=array;
-}
-
-int utf8(unsigned char *str,unsigned short *buffer)
+int utf8(unsigned char *str, unsigned short *buffer)
 {
   int c;
-  unsigned char *p=str;
-  unsigned short *q=buffer;
-  int n=0;
-  int expect=0;
-  int collect=0;
-  while((c=*p++)!=0)
+  unsigned char *p = str;
+  unsigned short *q = buffer;
+  int n = 0;
+  int expect = 0;
+  int collect = 0;
+  while ((c = *p++) != 0)
   {
-    if(expect==0)
+    if (expect == 0)
     {
-      if((c>>7)==0)
+      if ((c >> 7) == 0)
       {
         n++;
-        if(q!=NULL)
-          *q++=c;
-      } else if((c>>6)==2)
+        if (q != NULL)
+          *q++ = c;
+      } else if ((c >> 6) == 2)
       {
         n++;
-        if(q!=NULL)
-          *q++=0xFFDC;
-      } else if(c>>5==6)
+        if (q != NULL)
+          *q++ = 0xFFDC;
+      } else if (c >> 5 == 6)
       {
-        expect=1;
-        collect=c&0x1f;
-      }
-      else if(c>>4==14)
+        expect = 1;
+        collect = c & 0x1f;
+      } else if (c >> 4 == 14)
       {
-        expect=2;
-        collect=c&0x0f;
-      }
-      else if(c>>3==30)
+        expect = 2;
+        collect = c & 0x0f;
+      } else if (c >> 3 == 30)
       {
-        expect=3;
-        collect=c&0x07;
-      }
-      else
+        expect = 3;
+        collect = c & 0x07;
+      } else
       {
         n++;
-        if(q!=NULL)
-          *q++=0xfffd;;
+        if (q != NULL)
+          *q++ = 0xfffd;
+        ;
       }
     } else
     {
-      if(c>>6!=2)
+      if (c >> 6 != 2)
       {
         n++;
-        if(q!=NULL)
-          *q++=0xfffd;
-        expect=0;
+        if (q != NULL)
+          *q++ = 0xfffd;
+        expect = 0;
       } else
       {
-        collect<<=6;
-        collect|=(c&0x3f);
+        collect <<= 6;
+        collect |= (c & 0x3f);
         expect--;
-        if(expect==0)
+        if (expect == 0)
         {
           n++;
-          if(q!=NULL)
+          if (q != NULL)
           {
-            if(collect>0xffff)
-              collect=0xfffd;
-            *q++=collect;
+            if (collect > 0xffff)
+              collect = 0xfffd;
+            *q++ = collect;
           }
         }
       }
@@ -106,9 +82,40 @@ int utf8(unsigned char *str,unsigned short *buffer)
   }
   return n;
 }
-int main(int argc, char **argv)
+void **entry(int argc, unsigned char **argv)
 {
-  short buffer[2];
-  printf("%d\n",utf8(argv[1],buffer));
-  printf("%02x %02x\n",buffer[0],buffer[1]);
+  /*
+   * Allocate storage for array of Strings
+   */
+  void **result=(void **)heapptr;
+  heapptr+=WORDSIZE*argc+WORDSIZE;
+  *(int *)result=argc;
+  for(int i=0;i<argc;i++)
+  {
+    int size=utf8(argv[i],NULL);
+    /*
+     * Allocate storage for String
+     */
+    result[i+1] = heapptr;
+    heapptr += WORDSIZE;
+    /*
+     * Allocate storage for the char array
+     */
+    void *array = heapptr;
+    heapptr += WORDSIZE + size * 2;
+    /*
+     * Keep heap on WORD boundary
+     */
+    heapptr = (void *)((int)(heapptr + WORDSIZE - 1) & WORDMASK);
+    /*
+     * Write array
+     */
+    *((int *)array)=size;
+    utf8(argv[i],(unsigned short *)(array+WORDSIZE));
+    /*
+     * Store array in String
+     */
+    *((void **) result[i+1]) = array;
+  }
+  return result;
 }
