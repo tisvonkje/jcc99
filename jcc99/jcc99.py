ccd234a3ae12ee6cb81fa1a8268c664763812ed9
Franc Grootjen <sparky@Francs-iMac.local>
2019-07-25 22:21:22 +0200

    added lookup function

--- jcc99/jcc99.py
+++ jcc99/jcc99.py
@@ -9,29 +9,38 @@ class Jcc99:
     def __init__(self):
         pass
     
-    def do(self, debugger, command, result, internal_dict):
+    def cf(self, debugger, command, result, internal_dict):
       target=debugger.GetSelectedTarget()
       process=target.GetProcess()
       thread=process.GetSelectedThread()
       frame=thread.GetSelectedFrame()
-      for symbol in frame.GetModule():
-          print symbol.GetName()
+
       print frame.FindRegister("esp")
+      
       pc=int(frame.FindRegister("pc").value,0)
+      print self.lookup(pc,frame.GetModule())
+      
       error = lldb.SBError()
       uint = process.ReadUnsignedFromMemory(pc, 1, error)
       if error.Success():
         print('integer: %u' % uint)
       else:
         print('error: ', error)
-      print frame.FindRegister("eax")    
+      print frame.FindRegister("eax")
+      
+    def lookup(self, address, module):
+        for symbol in module:
+          if symbol.GetStartAddress().__int__()<= address and symbol.GetEndAddress().__int__()> address:
+              return symbol
+        return None
+            
 
-def do(debugger, command, result, internal_dict):
+def cf(debugger, command, result, internal_dict):
     global instance
-    instance.do(debugger,command,result,internal_dict)
+    instance.cf(debugger,command,result,internal_dict)
 
 # And the initialization code to add your commands
 def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand('command script add -f jcc99.do do')
+    debugger.HandleCommand('command script add -f jcc99.cf cf')
     
 instance=Jcc99()
