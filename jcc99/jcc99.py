2c863f684994fe4d7c4c70e81ec59260526ce6c8
Franc Grootjen <franc@me.com>
2019-07-25 17:25:17 +0200

    Now with a class :)

--- jcc99/jcc99.py
+++ jcc99/jcc99.py
@@ -5,28 +5,33 @@ import commands
 import optparse
 import shlex
 
-def do(debugger, command, result, internal_dict):
-    target=debugger.GetSelectedTarget()
-    process=target.GetProcess()
-    thread=process.GetSelectedThread()
-    frame=thread.GetSelectedFrame()
-    for symbol in frame.GetModule():
-        print symbol.GetName()
-    print frame.FindRegister("esp")
-    pc=int(frame.FindRegister("pc").value,0)
-    error = lldb.SBError()
-    uint = process.ReadUnsignedFromMemory(pc, 1, error)
-    if error.Success():
-      print('integer: %u' % uint)
-    else:
-      print('error: ', error)
-    print frame.FindRegister("eax")
+class Jcc99:
+    def __init__(self):
+        pass
+    
+    def do(self, debugger, command, result, internal_dict):
+      target=debugger.GetSelectedTarget()
+      process=target.GetProcess()
+      thread=process.GetSelectedThread()
+      frame=thread.GetSelectedFrame()
+      for symbol in frame.GetModule():
+          print symbol.GetName()
+      print frame.FindRegister("esp")
+      pc=int(frame.FindRegister("pc").value,0)
+      error = lldb.SBError()
+      uint = process.ReadUnsignedFromMemory(pc, 1, error)
+      if error.Success():
+        print('integer: %u' % uint)
+      else:
+        print('error: ', error)
+      print frame.FindRegister("eax")    
 
-def ls(debugger, command, result, internal_dict):
-    print 'hoi'
+def do(debugger, command, result, internal_dict):
+    global instance
+    instance.do(debugger,command,result,internal_dict)
 
 # And the initialization code to add your commands
 def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand('command script add -f jcc99.ls ls')
     debugger.HandleCommand('command script add -f jcc99.do do')
-    print 'The "ls" python command has been installed and is ready for use.'
+    
+instance=Jcc99()
