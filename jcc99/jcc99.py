bc6cbc4f6e0e38398d3afa1b1c6e78cccd5811e7
Franc Grootjen <franc@me.com>
2019-08-01 18:22:04 +0200

    reading parameters

--- jcc99/jcc99.py
+++ jcc99/jcc99.py
@@ -5,50 +5,93 @@ import commands
 import optparse
 import shlex
 
+
 class Jcc99:
+
     def __init__(self):
         pass
     
     def cf(self, debugger, command, result, internal_dict):
-      target=debugger.GetSelectedTarget()
-      process=target.GetProcess()
-      thread=process.GetSelectedThread()
-      frame=thread.GetSelectedFrame()
-      module=frame.GetModule()
+      target = debugger.GetSelectedTarget()
+      process = target.GetProcess()
+      thread = process.GetSelectedThread()
+      frame = thread.GetSelectedFrame()
+      module = frame.GetModule()
 #
 # Get pc and lookup where we are
 #     
-      pc=int(frame.FindRegister("pc").value,0)
-      fp=int(frame.FindRegister("ebp").value,0)
-      error=lldb.SBError()
+      pc = int(frame.FindRegister("pc").value, 0)
+      fp = int(frame.FindRegister("ebp").value, 0)
+      error = lldb.SBError()
       
       while True:
-        here=self.lookup(pc,module)
+        here = self.lookup(pc, module)
         if here is None:
           break
         else:
-          offset=pc-here.GetStartAddress().__int__()
-          print here.GetName()+"+"+str(offset)
-          pc=process.ReadUnsignedFromMemory(fp+4,4,error)
+          offset = pc - here.GetStartAddress().__int__()
+          #
+          # Check if we are in a Method
+          #
+          if here.GetName().startswith("Method_"):
+            #
+            # Try to find the debug info for this symbol
+            #
+            debug=module.FindSymbol("Debug_"+here.GetName()[7:])
+            if debug is None:
+              print here.GetName+"+"+str(offset)+"(?)"
+            else:
+              p=debug.GetStartAddress().__int__()
+              n=process.ReadUnsignedFromMemory(p,4,error)
+              p+=4
+              if error.Fail():
+                print here.GetName+"+"+str(offset)+"(?)"
+              else:
+                parameters=""
+                par=fp+4
+                for i in range(n):
+                  value=process.ReadUnsignedFromMemory(par,4,error)
+                  typeId=process.ReadUnsignedFromMemory(p,4,error)
+                  p+=4
+                  if typeId==7:
+                    parameters=parameters+str(value)
+                    par+=4
+                  elif typeId==2:
+                    if value!=0:
+                      parameters=parameters+"true"
+                    else:
+                      parameters=parameters+"false"
+                    par+=4
+                  else:
+                    parameters=parameters+hex(value)
+                    par+=4
+                  if i!=n-1:
+                    parameters=parameters+","
+                print "!" + here.GetName() + "+" + str(offset)+"("+parameters+")"
+          else:
+            print here.GetName() + "+" + str(offset)    
+          pc = process.ReadUnsignedFromMemory(fp + 4, 4, error)
           if error.Fail():
             break
-          fp=process.ReadUnsignedFromMemory(fp,4,error)
+          fp = process.ReadUnsignedFromMemory(fp, 4, error)
           if error.Fail():
             break
       
     def lookup(self, address, module):
         for symbol in module:
-          if symbol.GetStartAddress().__int__()<= address and symbol.GetEndAddress().__int__()> address:
+          if symbol.GetStartAddress().__int__() <= address and symbol.GetEndAddress().__int__() > address:
               return symbol
         return None
             
 
 def cf(debugger, command, result, internal_dict):
     global instance
-    instance.cf(debugger,command,result,internal_dict)
+    instance.cf(debugger, command, result, internal_dict)
+
 
 # And the initialization code to add your commands
 def __lldb_init_module(debugger, internal_dict):
     debugger.HandleCommand('command script add -f jcc99.cf cf')
+
     
-instance=Jcc99()
+instance = Jcc99()
